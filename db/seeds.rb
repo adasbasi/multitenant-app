91026ada327d1b8adc9532962aad149b0da6902a
adasbasi <dasbasi.ali@gmail.com>
2017-11-21 00:01:51 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 Blog.all.destroy_all
 Post.all.destroy_all
 
-Blog.create(id: 0, name: "My Example Blog", subdomain: "example")
-Blog.create(id: 1, name: "Awesome Blog", subdomain: "awesome")
+Blog.create(name: "My Example Blog", subdomain: "example")
+Blog.create(name: "Awesome Blog", subdomain: "awesome")
 
 Post.create(title:"Gönderi bir", body:"Deneme iletisi", blog_id: 0)
 Post.create(title:"Gönderi iki", body:"Deneme iletisi iki", blog_id: 1)
